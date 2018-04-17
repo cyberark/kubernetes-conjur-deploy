5ed5fed2784a4ba69a608c3a47555b06338aa1d6
John Tuttle <jtuttle@conjur.net>
2018-04-17 13:24:44 -0400

    add service address back to readme

--- 7_print_config.sh
+++ 7_print_config.sh
@@ -8,10 +8,12 @@ set_namespace $CONJUR_NAMESPACE_NAME
 announce "
 Conjur cluster is ready.
 
+Conjur master service:
+  conjur-master.$CONJUR_NAMESPACE_NAME.svc.cluster.local
+
 Conjur UI address:
   https://$(get_master_service_ip):443
 
 Conjur admin credentials:
   admin / $(rotate_api_key)
 "
-
