47bd66acc9fe5886f69a9e240e7f38105265d05a
John Tuttle <jtuttle@conjur.net>
2018-06-12 15:21:29 -0400

    modify image tags

--- test.sh
+++ test.sh
@@ -45,13 +45,16 @@ function setupTestEnvironment() {
 
 function buildDockerImages() {
   # Conjur appliance retagged for K8s / OpenShift registry.
-  local appliance_image="registry2.itci.conjur.net/conjur-appliance:4.9-stable"
-  export CONJUR_APPLIANCE_IMAGE="$DOCKER_REGISTRY_PATH/conjur-appliance:$CONJUR_NAMESPACE_NAME"
-  docker pull $appliance_image
-  docker tag $appliance_image $CONJUR_APPLIANCE_IMAGE
+#  local appliance_image="registry2.itci.conjur.net/conjur-appliance:4.9-stable"
+#  export CONJUR_APPLIANCE_IMAGE="$DOCKER_REGISTRY_PATH/conjur-appliance:$CONJUR_NAMESPACE_NAME"
+#  docker pull $appliance_image
+    #  docker tag $appliance_image $CONJUR_APPLIANCE_IMAGE
+
+  export CONJUR_APPLIANCE_IMAGE="registry2.itci.conjur.net/conjur-appliance:4.9-stable"
 
   # Test image w/ kubectl and oc CLIs installed to drive scripts.
-  export K8S_CONJUR_DEPLOY_TESTER_IMAGE="${DOCKER_REGISTRY_PATH}/k8s-conjur-deploy-tester:$CONJUR_NAMESPACE_NAME"    
+#  export K8S_CONJUR_DEPLOY_TESTER_IMAGE="${DOCKER_REGISTRY_PATH}/k8s-conjur-deploy-tester:$CONJUR_NAMESPACE_NAME"
+  export K8S_CONJUR_DEPLOY_TESTER_IMAGE="k8s-conjur-deploy-tester:$CONJUR_NAMESPACE_NAME"
   docker build --build-arg OPENSHIFT_CLI_URL=$OPENSHIFT_CLI_URL \
     -t $K8S_CONJUR_DEPLOY_TESTER_IMAGE -f Dockerfile.test .
 }
