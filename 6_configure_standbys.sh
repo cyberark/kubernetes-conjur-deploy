41189947a44d40d07cbcc50290e58739ff2350c6
John Tuttle <jtuttle@conjur.net>
2018-06-13 11:00:59 -0400

    use different tmp folder for seed

--- 6_configure_standbys.sh
+++ 6_configure_standbys.sh
@@ -11,11 +11,11 @@ master_pod_name=$(get_master_pod_name)
 
 echo "Preparing standby seed files..."
 
-mkdir -p tmp
+mkdir -p seed-tmp
 
 ls
 
-$cli exec $master_pod_name evoke seed standby conjur-standby > ./tmp/standby-seed.tar
+$cli exec $master_pod_name evoke seed standby conjur-standby > ./seed-tmp/standby-seed.tar
 
 master_pod_ip=$($cli describe pod $master_pod_name | awk '/IP:/ { print $2 }')
 pod_list=$($cli get pods -l role=unset --no-headers | awk '{ print $1 }')
@@ -25,13 +25,13 @@ for pod_name in $pod_list; do
 
   $cli label --overwrite pod $pod_name role=standby
     
-  copy_file_to_container "./tmp/standby-seed.tar" "/tmp/standby-seed.tar" "$pod_name"
+  copy_file_to_container "./seed-tmp/standby-seed.tar" "/seed-tmp/standby-seed.tar" "$pod_name"
 
-  $cli exec $pod_name evoke unpack seed /tmp/standby-seed.tar
+  $cli exec $pod_name evoke unpack seed /seed-tmp/standby-seed.tar
   $cli exec $pod_name -- evoke configure standby -i $master_pod_ip
 done
 
-rm -rf tmp
+rm -rf seed-tmp
 
 echo "Standbys configured."
 echo "Starting synchronous replication..."
--- 7_configure_followers.sh
+++ 7_configure_followers.sh
@@ -11,8 +11,8 @@ master_pod_name=$(get_master_pod_name)
 
 echo "Preparing follower seed files..."
 
-mkdir -p tmp
-$cli exec $master_pod_name evoke seed follower conjur-follower > ./tmp/follower-seed.tar
+mkdir -p seed-tmp
+$cli exec $master_pod_name evoke seed follower conjur-follower > ./seed-tmp/follower-seed.tar
 
 master_pod_ip=$($cli describe pod $master_pod_name | awk '/IP:/ { print $2 }')
 pod_list=$($cli get pods -l role=follower --no-headers | awk '{ print $1 }')
@@ -20,12 +20,12 @@ pod_list=$($cli get pods -l role=follower --no-headers | awk '{ print $1 }')
 for pod_name in $pod_list; do
   printf "Configuring follower %s...\n" $pod_name
 
-  copy_file_to_container "./tmp/follower-seed.tar" "/tmp/follower-seed.tar" "$pod_name"
+  copy_file_to_container "./seed-tmp/follower-seed.tar" "/seed-tmp/follower-seed.tar" "$pod_name"
 
-  $cli exec $pod_name evoke unpack seed /tmp/follower-seed.tar
+  $cli exec $pod_name evoke unpack seed /seed-tmp/follower-seed.tar
   $cli exec $pod_name -- evoke configure follower
 done
 
-rm -rf tmp
+rm -rf seed-tmp
 
 echo "Followers configured."
