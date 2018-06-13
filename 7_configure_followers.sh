dc598215041fca9fb969b750191be87d9ca62555
John Tuttle <jtuttle@conjur.net>
2018-06-13 14:37:17 -0400

    return to creating seed once

--- 7_configure_followers.sh
+++ 7_configure_followers.sh
@@ -12,6 +12,7 @@ master_pod_name=$(get_master_pod_name)
 echo "Preparing follower seed files..."
 
 mkdir -p tmp
+$cli exec $master_pod_name evoke seed follower conjur-follower > ./tmp/follower-seed.tar
 
 master_pod_ip=$($cli describe pod $master_pod_name | awk '/IP:/ { print $2 }')
 pod_list=$($cli get pods -l role=follower --no-headers | awk '{ print $1 }')
@@ -19,7 +20,6 @@ pod_list=$($cli get pods -l role=follower --no-headers | awk '{ print $1 }')
 for pod_name in $pod_list; do
   printf "Configuring follower %s...\n" $pod_name
 
-  $cli exec $master_pod_name evoke seed follower conjur-follower > ./tmp/follower-seed.tar
   copy_file_to_container "./tmp/follower-seed.tar" "/tmp/follower-seed.tar" "$pod_name"
 
   $cli exec $pod_name evoke unpack seed /tmp/follower-seed.tar
