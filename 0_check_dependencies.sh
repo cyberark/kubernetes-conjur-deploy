aa4080e461ab13134f51c6c4f6b824683cff9986
Srdjan Grubor <sgnn7@sgnn7.org>
2018-12-17 14:47:37 -0600

    Added validation of seed file paths
    We now ensure that the seed file is an actual file before continuing the
    installation process.

--- 0_check_dependencies.sh
+++ 0_check_dependencies.sh
@@ -20,7 +20,7 @@ fi
 appliance_tag=${CONJUR_APPLIANCE_IMAGE//[A-Za-z.]*:/}
 appliance_version=${appliance_tag//\.[0-9A-Za-z.-]*/}
 if [ "${appliance_version}" != "$CONJUR_VERSION" ]; then
-  echo "Your appliance does not match the specified Conjur version."
+  echo "ERROR! Your appliance does not match the specified Conjur version."
   exit 1
 fi
 
@@ -32,4 +32,9 @@ fi
 
 if [[ "${DEPLOY_MASTER_CLUSTER}" = "false" ]]; then
   check_env_var "FOLLOWER_SEED_PATH"
+
+  if [[ ! -f "${FOLLOWER_SEED_PATH}" ]]; then
+    echo "ERROR! Follower seed path '${FOLLOWER_SEED_PATH}' does not point to a file!"
+    exit 1
+  fi
 fi
