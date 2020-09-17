785cf9d63de367d89ef668e9b2879f4f7137e352
Micah Lee <micah.lee@cyberark.com>
2020-09-17 14:56:49 -0400

    DEV! Test updated image

--- test.sh
+++ test.sh
@@ -54,7 +54,7 @@ function setupTestEnvironment() {
 }
 
 function buildDockerImages() {
-  export CONJUR_APPLIANCE_IMAGE="registry.tld/conjur-appliance:5.0-stable"
+  export CONJUR_APPLIANCE_IMAGE="registry.tld/conjur-appliance:5.11.0-20200917173708-1cd3e85"
 
   docker pull $CONJUR_APPLIANCE_IMAGE
 
